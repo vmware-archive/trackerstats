f259376f5c0e0982380439eae1d2a83996a438c7
Winston <winston.yongwei@gmail.com>
2011-10-14 01:09:04 +0800

    Basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
+
   def index
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "pivotallabs" && password == "pivotal8tracker"
+    end
   end
 end
