a06d11e10b3a2afa1b35db58f5bc797b80024e1d
Winston <winston.yongwei@gmail.com>
2011-10-13 21:42:14 +0800

    Getting started.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails' , '3.1.1'
+gem 'haml'  , '~> 3.1.3'
+
+gem 'pivotal-tracker' , '~> 0.4.1'
+gem 'google_visualr'  , '>= 2.1.0'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracker::Application.config.secret_token = 'a13dbb2b274e761a370d9db4c35b241e75ba55f27d36fdd4e5bb2aee3cd389b914a2f17a831ff4132895f9d745cf039ebef64779c0e8802ec2e3a9ca063914e7'
