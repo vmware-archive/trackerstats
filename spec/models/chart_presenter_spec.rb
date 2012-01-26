25e41235f7e74562a42274364aa2bd4da3eef38d
Diego Garcia & Tomasz Maszkowski <pair+diego+tomasz@pivotallabs.com>
2012-01-26 17:54:21 +0800

    WIP. Refactoring chart presenter

--- spec/models/chart_presenter_spec.rb
+++ spec/models/chart_presenter_spec.rb
@@ -1,6 +1,60 @@
 require 'spec_helper'
 
 describe ChartPresenter do
+  before do
+    @project = double("project")
+
+    @sample_stories = [
+      double(
+          :story_type => story_type,
+          :created_at => DateTime.parse("2011-01-03 00:01:00 Z"), # week 1
+          :current_state => "accepted",
+          :accepted_at => DateTime.parse("2011-01-28 00:02:00 Z")), # week 4
+      double(
+          :story_type => story_type,
+          :created_at => DateTime.parse("2011-01-08 00:01:00 Z"), # week 2
+          :current_state => "started"),
+      double(
+          :story_type => story_type,
+          :created_at => DateTime.parse("2011-01-15 00:01:00 Z"), # week 3
+          :current_state => "accepted",
+          :accepted_at => DateTime.parse("2011-01-21 00:02:00 Z")), # week 3
+      double(
+          :story_type => story_type,
+          :created_at => DateTime.parse("2011-01-22 00:01:00 Z"), # week 4
+          :current_state => "started")
+    ]
+
+    stories = double("project stories")
+    stories.stub(:all).and_return(@sample_stories)
+
+    @project.stub(:stories).and_return(stories)
+
+    iterations = double("iterations")
+    iterations.stub(:all).and_return([
+      double(
+        :number => 1,
+        :start => Date.parse("2011-01-03"),
+        :finish => Date.parse("2011-01-10")),
+      double(
+        :number => 2,
+        :start => Date.parse("2011-01-10"),
+        :finish => Date.parse("2011-01-17")),
+      double(
+        :number => 3,
+        :start => Date.parse("2011-01-17"),
+        :finish => Date.parse("2011-01-24")),
+      double(
+        :number => 4,
+        :start => Date.parse("2011-01-24"),
+        :finish => Date.parse("2011-01-31")),
+    ])
+
+    @project.stub(:iterations).and_return(iterations)
+
+    @chart = ChartPresenter.new(@sample_stories, Date.parse('2011-01-01'))
+  end
+
   shared_examples_for "a chart generation method" do
     it "allows the chart name to be set" do
       data_table = @chart.send(chart_type, "My Title")
@@ -35,7 +89,7 @@ describe ChartPresenter do
 
       rows.length.should == 2
 
-      row_values(rows, 0).should == [1, 27]
+      row_values(rows, 0).should == [1, 25]
       row_values(rows, 1).should == [3, 6]
     end
   end
@@ -44,25 +98,11 @@ describe ChartPresenter do
     it "produces a bar chart for the time to acceptance of each story_type" do
       rows = rows_for_chart(chart_type)
 
-      rows.length.should == 28
+      rows.length.should == 26
 
       row_values(rows, 6).should  == ["6", 1]
-      row_values(rows, 27).should == ["27", 1]
+      row_values(rows, 25).should == ["25", 1]
     end
-  end
-
-  before do
-    @sample_stories = [
-      PivotalTracker::Story.new(:story_type => story_type, :created_at => DateTime.parse("2011-01-01 00:01:00 Z"), # week 1
-                                :current_state => "accepted", :accepted_at => DateTime.parse("2011-01-28 00:02:00 Z")), # week 4
-      PivotalTracker::Story.new(:story_type => story_type, :created_at => DateTime.parse("2011-01-08 00:01:00 Z"), # week 2
-                                :current_state => "started"),
-      PivotalTracker::Story.new(:story_type => story_type, :created_at => DateTime.parse("2011-01-15 00:01:00 Z"), # week 3
-                                :current_state => "accepted", :accepted_at => DateTime.parse("2011-01-21 00:02:00 Z")), # week 3
-      PivotalTracker::Story.new(:story_type => story_type, :created_at => DateTime.parse("2011-01-22 00:01:00 Z"), # week 4
-                                :current_state => "started")
-    ]
-    @chart = ChartPresenter.new(@sample_stories, Date.parse('2011-01-01'))
   end
 
   context "features" do
@@ -142,6 +182,54 @@ describe ChartPresenter do
       it_should_behave_like "a chart generation method"
 
       it_should_behave_like "story_type_acceptance_total_by_days"
+    end
+  end
+
+
+  context "use iteration number" do
+    let(:story_type) {"feature"}
+    before do
+      @project = double("project")
+      stories = double("project stories")
+      stories.stub(:all).and_return(@sample_stories)
+
+      @project.stub(:stories).and_return(stories)
+
+      it1 = double("it1")
+      it1.stub(:start).and_return(Date.parse("2011-01-03"))
+      it1.stub(:finish).and_return(Date.parse("2011-01-10"))
+      it1.stub(:number).and_return(1)
+
+      it2 = double("it2")
+      it2.stub(:start).and_return(Date.parse("2011-01-10"))
+      it2.stub(:finish).and_return(Date.parse("2011-01-17"))
+      it2.stub(:number).and_return(2)
+
+      it3 = double("it3")
+      it3.stub(:start).and_return(Date.parse("2011-01-17"))
+      it3.stub(:finish).and_return(Date.parse("2011-01-24"))
+      it3.stub(:number).and_return(3)
+
+      it4 = double("it4")
+      it4.stub(:start).and_return(Date.parse("2011-01-24"))
+      it4.stub(:finish).and_return(Date.parse("2011-01-31"))
+      it4.stub(:number).and_return(3)
+
+      iterations = double("iterations")
+      iterations.stub(:all).and_return([it1, it2, it3, it4])
+
+      @project.stub(:iterations).and_return(iterations)
+    end
+
+    describe "test" do
+      it "project should have iterations" do
+        @project.iterations.should_not be_nil
+        @project.iterations.should respond_to(:all)
+        @project.iterations.all.count.should  == 4
+
+        @project.stories.should respond_to(:all)
+        @project.stories.all.count.should == 4
+      end
     end
   end
 
